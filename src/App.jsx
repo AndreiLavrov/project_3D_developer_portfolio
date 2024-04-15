8eb31ae1e85f1c1aa9491562979f15ef56122a65
Andrei Lavrov <lavrovvandrei@gmail.com>
2024-04-15 19:52:57 +0300

    Delete "react-tilt" usage for successful build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { Contact, Experience, Feedbacks, Hero, Navbar, Tech, StarsCanvas } from "./components";
 
 const App = () => {
   return (
@@ -10,10 +10,8 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-        <About />
         <Experience />
         <Tech />
-        <Works />
         <Feedbacks />
         <div className='relative z-0'>
           <Contact />
